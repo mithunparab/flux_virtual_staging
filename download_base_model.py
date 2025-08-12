74f4fc773bb6dc50b3329e19d3eba79778c56f2a
Mithun Parab <mithunprb@gmail.com>
2025-08-13 01:39:31 +0530

    updated download_base_model.py

--- download_base_model.py
+++ download_base_model.py
@@ -3,10 +3,6 @@ from huggingface_hub import snapshot_download
 from pathlib import Path
 
 if __name__ == "__main__":
-    hf_token = os.getenv("HUGGING_FACE_HUB_TOKEN")
-
-    if not hf_token:
-        raise ValueError("FATAL: HUGGING_FACE_HUB_TOKEN environment variable not found.")
 
     model_name = "black-forest-labs/FLUX.1-Kontext-dev"
     local_path = Path("./models/flux-dev-kontext")
@@ -15,7 +11,6 @@ if __name__ == "__main__":
     snapshot_download(
         repo_id=model_name,
         local_dir=local_path,
-        token=hf_token,  
         local_dir_use_symlinks=False,
         ignore_patterns=["*.safetensors", "*.onnx", "*.bin"]
     )
